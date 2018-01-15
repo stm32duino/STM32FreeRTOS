55a81070ede03b972c44d286ae4d7fef5e96d6fa
Frederic Pillon <frederic.pillon@st.com>
2018-01-15 15:40:19 +0100

    Added config for frBlinkPrint example
    Required config:
    #define INCLUDE_uxTaskGetStackHighWaterMark 1
    #define INCLUDE_xTaskGetIdleTaskHandle 1
    
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

--- src/FreeRTOSConfig_Default.h
+++ src/FreeRTOSConfig_Default.h
@@ -135,6 +135,8 @@ to exclude the API function. */
 #define INCLUDE_vTaskDelayUntil        1
 #define INCLUDE_vTaskDelay             1
 #define INCLUDE_xTaskGetSchedulerState 1
+#define INCLUDE_uxTaskGetStackHighWaterMark 1
+#define INCLUDE_xTaskGetIdleTaskHandle 1
 
 /* Cortex-M specific definitions. */
 #ifdef __NVIC_PRIO_BITS
