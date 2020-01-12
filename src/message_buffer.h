706fc688f4b8efc1e9163fbc678e1c508c0efc74
Erik Tideman <erik.tideman@gmail.com>
2020-01-12 07:55:53 +0100

    Added missing include wrapper for message_buffer.h

---
+++ src/message_buffer.h
@@ -0,0 +1,11 @@
+/*
+ * @file    message_buffer.h
+ * @author  Frederic Pillon <frederic.pillon@st.com> for STMicroelectronics.
+ * @brief   Include header file to match Arduino library format
+ */
+#ifndef _ARDUINO_MESSAGE_BUFFER_H_
+#define _ARDUINO_MESSAGE_BUFFER_H_
+
+#include "FreeRTOS/Source/include/message_buffer.h"
+
+#endif //_ARDUINO_MESSAGE_BUFFER_H_
