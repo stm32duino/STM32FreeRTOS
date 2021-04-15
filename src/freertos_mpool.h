ed97d17408883bbc8ae792a0daf20bc57cf1594d
Frederic Pillon <frederic.pillon@st.com>
2021-04-15 19:00:11 +0200

    Added new include wrapper
    Signed-off-by: Frederic Pillon <frederic.pillon@st.com>

---
+++ src/freertos_mpool.h
@@ -0,0 +1,11 @@
+/*
+ * @file    freertos_mpool.h
+ * @author  Frederic Pillon <frederic.pillon@st.com> for STMicroelectronics.
+ * @brief   Include header file to match Arduino library format
+ */
+#ifndef _ARDUINO_FREERTOS_MPOOL_H_
+#define _ARDUINO_FREERTOS_MPOOL_H_
+
+#include "../portable/CMSIS_RTOS_V2/freertos_mpool.h"
+
+#endif //_ARDUINO_FREERTOS_MPOOL_H_
---
+++ src/freertos_os2.h
@@ -0,0 +1,11 @@
+/*
+ * @file    freertos_os2.h
+ * @author  Frederic Pillon <frederic.pillon@st.com> for STMicroelectronics.
+ * @brief   Include header file to match Arduino library format
+ */
+#ifndef _ARDUINO_FREERTOS_OS2_H_
+#define _ARDUINO_FREERTOS_OS2_H_
+
+#include "../portable/CMSIS_RTOS_V2/freertos_os2.h"
+
+#endif //_ARDUINO_FREERTOS_OS2_H_
